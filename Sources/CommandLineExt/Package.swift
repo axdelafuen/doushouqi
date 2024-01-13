999be31a717411c44d3f9f3aed9dd8a67419b27f
Axel DE LA FUENTE <axel.dlf@free.fr>
2024-01-13 14:45:13 +0100

    :green_heart: add ci

--- Sources/CommandLineExt/Package.swift
+++ Sources/CommandLineExt/Package.swift
@@ -12,15 +12,16 @@ let package = Package(
             targets: ["CommandLineExt"]),
     ],
     dependencies: [
-        .package(name: "Model", path: "Model"),
+        .package(name: "Model", path: "../Model")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "CommandLineExt"),
+            name: "CommandLineExt",
+            dependencies: ["Model"]),
         .testTarget(
             name: "CommandLineExtTests",
-            dependencies: ["CommandLineExt"]),
+            dependencies: ["CommandLineExt", "Model"]),
     ]
 )

28df55a0bf134bfb8eed2c459126d5ae5a9d54f2
Axel DE LA FUENTE <axel.dlf@free.fr>
2024-01-30 12:43:43 +0100

    :hammer: update coverage script

--- Sources/Model/coverage.sh
+++ Sources/Model/coverage.sh
@@ -4,7 +4,7 @@ BIN_PATH="$(swift build --show-bin-path)"
 XCTEST_PATH="$(find ${BIN_PATH} -name '*.xctest')"
 
 COV_BIN=$XCTEST_PATH
-if [[ "$OSTYPE" == "darwin"* ]]; then
+if [ "$OSTYPE" == "darwin"* ]; then
     f="$(basename $XCTEST_PATH .xctest)"
     COV_BIN="${COV_BIN}/Contents/MacOS/$f"
 fi

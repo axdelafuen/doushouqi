cf8c39e6841bb58f9d69526699352907a186ee62
Axel DE LA FUENTE <axel.dlf@free.fr>
2024-02-08 11:18:39 +0100

    :bug: change some debug pb

--- Sources/CommandLineTest/CommandLineTest/main.swift
+++ Sources/CommandLineTest/CommandLineTest/main.swift
@@ -13,11 +13,8 @@ import CommandLineExt
 //debugTp2()
 //debugTp3()
 //debugTp4()
+//debugTp5()
 
 //try await two_dumb_AI_Game()
-
 //try await simple_AI_vs_random_Game()
-
 //try await testHumanVsRandom()
-
-debugTp5()

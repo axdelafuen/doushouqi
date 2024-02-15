578481ceb36077b99e93809744362d3a5dff2825
Axel DE LA FUENTE <axel.dlf@free.fr>
2024-02-15 11:33:32 +0100

    :sparkles: add player serialization to game

--- Sources/Persistence/Sources/Persistence/GamePersist.swift
+++ Sources/Persistence/Sources/Persistence/GamePersist.swift
@@ -1,8 +1,8 @@
 import Foundation
 import Model
-/*
+
 extension Game : Codable {
-    
+        
     enum Key: CodingKey {
         case rules
         case board
@@ -15,18 +15,38 @@ extension Game : Codable {
         
         try container.encode(rules.toData(), forKey: .rules)
         try container.encode(board, forKey: .board)
-        try container.encode(player1, forKey: .player1)
-        try container.encode(player2, forKey: .player2)
+        try container.encode(player1.toData(), forKey: .player1)
+        try container.encode(player2.toData(), forKey: .player2)
     }
     
     public init(from decoder: Decoder) throws {
         let value = try decoder.container(keyedBy: Key.self)
         let rules = try value.decode(RulesDatas.self, forKey: .rules)
         let board = try value.decode(Board.self, forKey: .board)
-        let player1 = try value.decode(Player.self, forKey: .player1)
-        let player2 = try value.decode(Player.self, forKey: .player2)
+        let dataPlayer1 = try value.decode(PlayerDatas.self, forKey: .player1)
+        let dataPlayer2 = try value.decode(PlayerDatas.self, forKey: .player2)
         
-        self.init(rules: rules.toObject()!, player1: player1, player2: player2, board: board)
+        let player1:Player?
+        let player2:Player?
+        
+        switch dataPlayer1.playerType{
+        case "HumanPlayer":
+            player1 = dataPlayer1.toObject(input: nil)
+        default:
+            player1 = dataPlayer1.toObject(input: nil)
+        }
+
+        switch dataPlayer2.playerType{
+        case "HumanPlayer":
+            player2 = dataPlayer2.toObject(input: nil)
+        default:
+            player2 = dataPlayer2.toObject(input: nil)
+        }
+        
+        if let player1, let player2 {
+            self.init(rules: rules.toObject()!, player1: player1, player2: player2, board: board)
+        }
+        self.init(rules: rules.toObject()!, player1: player1!, player2: player2!, board: board)
     }
 }
-*/
+
